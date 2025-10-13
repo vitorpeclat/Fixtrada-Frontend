4874cc20253384e4e5d33289727e1fd5c9e11e2d
Vitor Rodrigues Peclat <vitorr.peclat@gmail.com>
2025-10-12 21:15:20 -0300

    teste

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { registerRootComponent } from 'expo';
 
-import Login from './src/app/Login';
+import Login from './src/app/(auth)/Login';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
